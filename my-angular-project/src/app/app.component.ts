f87c6bd265672b32c23134420e97a24225c80568
Orkun Dayı <75010825+orkundayi@users.noreply.github.com>
2024-05-29 17:34:50 +0300

    main
    main

--- my-angular-project/src/app/app.component.ts
+++ my-angular-project/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
     selector: 'app-root',
@@ -10,5 +11,9 @@ import { UserComponent } from "./user/user.component";
     imports: [HeaderComponent, UserComponent]
 })
 export class AppComponent {
-  title = 'my-angular-project';
+  users = DUMMY_USERS;
+
+  onSelected(id: string) {
+    alert(`Selected user with id: ${id}`);
+  }
 }
--- my-angular-project/src/app/user/user.component.ts
+++ my-angular-project/src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+import { Component , EventEmitter, Input , Output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,5 +8,14 @@ const randomUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  user = randomUser;
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>();
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
+  }
+  onSelected() {
+    this.select.emit(this.id);
+  }
 }
